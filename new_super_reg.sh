2932d7a34d0b1de2e6171c0aff902b5f882ac01e
onarvaez <om.narv@gmail.com>
2019-02-06 12:14:39 -0600

    mrdegibbs added

--- new_super_reg.sh
+++ new_super_reg.sh
@@ -1,12 +1,13 @@
 dwiextract cat_all.mif -no_bzero no_bzero.mif
 dwiextract cat_all.mif -bzero bzero.mif
 mrcat bzero.mif no_bzero.mif dwi_pre.mif
-mrcrop dwi_pre.mif -axis 2 19 121 - 	| mrcrop - -axis 1 35 138 dwi_pre_crop.mif 
+mrcrop dwi_pre.mif -axis 2 19 121 - 	| mrcrop - -axis 1 35 138 dwi_pre_crop.mif
 dwidenoise dwi_pre_crop.mif dwi_denoised_crop.mif -noise noise.mif
-dwibiascorrect -ants dwi_denoised_crop.mif dwi_denoised_crop_unbias.mif -ants.b [7,3] -ants.c [700,0.0] -bias bias.mif 
+mrdegibbs dwi_denoised_crop.mif dwi_denoised_crop_gibbs.mif
+dwibiascorrect -ants dwi_denoised_crop_gibbs.mif dwi_denoised_crop_unbias.mif -ants.b [7,3] -ants.c [700,0.0] -bias bias.mif
 mrinfo dwi_denoised_crop_unbias.mif -export_grad_mrtrix b.txt -bvalue_scaling false
 dwiextract dwi_denoised_crop_unbias.mif  -bzero bzero_cropped.nii.gz
-fslsplit bzero_cropped.nii.gz 
+fslsplit bzero_cropped.nii.gz
 mrcat vol0000.nii.gz vol0001.nii.gz vol0002.nii.gz vol0003.nii.gz vol0004.nii.gz - | mrmath -axis 3 - mean avbzero_5.nii.gz
 fslmaths avbzero_5.nii.gz -thr 400 -bin avbzero_5_mask.nii.gz
 
@@ -63,13 +64,13 @@ mrcat vol*_mean_Warped.nii.gz b7000_cat.mif
 rm vol*
 mrmath -axis 3 b7000_cat.mif mean avb7000.nii.gz
 #modelos
-mrcat bzero_cat.mif b1000_cat.mif b3000_cat.mif b5000_cat.mif b7000_cat.mif dwi_no_grad.mif 
+mrcat bzero_cat.mif b1000_cat.mif b3000_cat.mif b5000_cat.mif b7000_cat.mif dwi_no_grad.mif
 
 mrconvert dwi_no_grad.mif  -grad b.txt dwi_proc.mif
 fslmaths avbzero.nii.gz -thr 400 -bin avbzero_mask.nii.gz
 
 dwi2tensor dwi_proc.mif -mask avbzero_mask.nii.gz dt.mif
-tensor2metric dt.mif -fa fa.mif -vector vector.mif -adc adc.mif -ad ad.mif -rd rd.mif 
+tensor2metric dt.mif -fa fa.mif -vector vector.mif -adc adc.mif -ad ad.mif -rd rd.mif
 
 dwi2response dhollander bet_grad.mif -info out_wm.txt out_gm.txt out_csf.txt
 dwi2fod msmt_csd dwi_proc.mif -mask avbzero_mask.nii.gz out_wm.txt fod_wm.mif out_gm.txt fod_gm.mif out_csf.txt fod_csf.mif
@@ -85,4 +86,3 @@ fixel2voxel disp.mif sum disp_sum.mif
 fixel2voxel disp.mif mean disp_mean.mif
 fixel2voxel disp.mif split_data disp_split.mif
 cd ..
-
